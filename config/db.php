e6fdc69e0e60133a6f0622f9e10e5b82fe0a597e
diablofong <diablofong@gmail.com>
2015-04-14 23:43:56 +0800

    project init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ezupload',
+    'username' => 'ezupload',
+    'password' => '!2#zaqwedcxs',
+    'charset' => 'utf8',
+];
---
+++ views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to use ezupload</h1>
+        <br>
+        <h1>file system</h1>
+    </div>
+
+</div>
